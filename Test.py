7fdc46db49e97dba139d26d7d311c893110181b7
bryan <ee@email.com>
2020-05-31 13:15:24 -0700

    initial commit

---
+++ Test.py
@@ -0,0 +1,31 @@
+# This is Hello World program. Hashtag make a comment so compiler ignores this line
+# print("Hello World!")
+# alt + f12 to open terminal
+# comment -> ctrl /
+
+# printing three other messages. Python prints in sequential order of the code
+# print("Ema the lion says hi!")
+# print("I need to not use the semicolon for Python")
+# print("I am writing notes.")
+
+# variables
+# x = 7
+# y = 6
+# z = x + y
+# print(z) # should be 13
+# print(x - y + 4) # should be 5
+
+# input from keyboard
+# celcius to farenheight
+f = float(input("Enter farenheight value: "))
+c = (f - 32) * (5 / 9)
+print(c)
+
+# num input
+num = int(input("Enter a number between 1 and 100: "))
+print(num)
+
+# string input
+# inputs are automatically considered as strings, so no need to cast
+name = input("Enter your name: ")
+print(name)
